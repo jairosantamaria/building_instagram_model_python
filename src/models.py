0aa0bdd79622990d4c0acbbe172bb9ef52b3f309
jairosantamaria <jairosantamaria@amelearning.net>
2021-03-27 03:15:00 +0000

    rev02

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,32 +8,35 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+#class Address(Base):
+   # __tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    #street_name = Column(String(250))
+    #street_number = Column(String(250))
+    #post_code = Column(String(250), nullable=False)
+    #person_id = Column(Integer, ForeignKey('person.id'))
+    #person = relationship(Person)
 
 class Media(Base):
     __tablename__ = 'media'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    tipo = Column(Enum(ABC))
-    url = Column(String(250))
-    post_id = Column(Integer, nullable=False)
+    tipo = Column(Enum)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('person.id'))
+    post = relationship(Post)
        
 class Post(Base):
     __tablename__ = 'post'
@@ -47,9 +50,22 @@ class Comment(Base):
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250))
+    comment_text = Column(String(250), nullable=False)
     author_id = Column(Integer, ForeignKey('person.id'))
     post_id = Column(Integer, ForeignKey('person.id'))
+    user = relationship(User)
+    post = relationship(Post)
+
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_from_id = (Integer, ForeignKey('person.id'))
+    user_to_id = (Integer, ForeignKey('person.id'))
+    follower = relationship(User)
+    
     
     def to_dict(self):
         return {}
